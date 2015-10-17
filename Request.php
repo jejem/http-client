9afd9dc09cb75d51b2d0d5710c56e29d360bf813
Jérémy Desvages <jejem@phyrexia.org>
2015-10-17 14:59:53 +0200

    Release v1.3.0

--- Request.php
+++ Request.php
@@ -5,7 +5,7 @@
  * @author Jérémy 'Jejem' Desvages <jejem@phyrexia.org>
  * @copyright Jérémy 'Jejem' Desvages
  * @license The MIT License (MIT)
- * @version 1.2.0
+ * @version 1.3.0
 **/
 
 namespace Phyrexia\Http;
