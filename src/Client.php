03f272a8e17a8d22c707ac0ed048c925c9b48ff3
Jérémy Desvages <jejem@phyrexia.org>
2015-10-25 16:57:59 +0100

    Using PSR-7 Request/Response objects

--- src/Client.php
+++ src/Client.php
@@ -11,183 +11,109 @@
 namespace Phyrexia\Http;
 
 class Client {
-	private $url;
-	private $method = 'GET';
+	private $request;
+	private $response;
+	private $options = array();
 
-	private $contentType = 'application/x-www-form-urlencoded';
-	private $userAgent;
-	private $postFields;
+	private $errno;
+	private $error;
+
 	private $followRedirects = true;
+	private $maxTries = 5;
 
-	private $responseCode;
-	private $responseContentType;
-	private $requestHeader;
-	private $responseFull;
-	private $responseHeaderSize;
-	private $responseHeader;
-	private $responseBody;
+	public function __construct($url = NULL, $method = 'GET', array $options = array()) {
+		if (! is_null($url)) {
+			$this->request = new Request($method, $url);
+			$this->request = $this->request->withHeader('Expect', '');
+			$this->request = $this->request->withHeader('Connection', 'close');
+			$this->request = $this->request->withHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+		}
 
-	private $responseErrno;
-	private $responseError;
-
-	private $maxRequests = 5;
-	private $connectTimeout = 5;
-	private $requestTimeout = 10;
-
-	public function __construct(Uri $url, $method = 'GET', array $options = array()) {
-		$this->setUrl($url);
-		$this->setMethod($method);
-
-		foreach ($options as $k => $v)
-			$this->$k = $v;
+		$this->options = $options;
 	}
 
-	public function __toString() {
-		return print_r($this, true);
+	public function getRequest() {
+		return $this->request;
 	}
 
-	private function reset() {
-		$this->contentType = 'application/x-www-form-urlencoded';
-		$this->userAgent = NULL;
-		$this->postFields = NULL;
-		$this->followRedirects = true;
+	public function setRequest(\Psr\Http\Message\RequestInterface $request) {
+		$this->request = $request;
+
+		return $this;
 	}
 
-	public function setUrl(Uri $url) {
-		if (! filter_var((string)$url, FILTER_VALIDATE_URL))
-			return false;
+	public function getResponse() {
+		return $this->response;
+	}
 
-		$this->url = $url;
+	public function setResponse(\Psr\Http\Message\ResponseInterface $response) {
+		$this->response = $response;
 
-		return true;
+		return $this;
+	}
+
+	public function getErrno() {
+		return $this->errno;
+	}
+
+	public function getError() {
+		return $this->error;
 	}
 
 	public function setMethod($method) {
-		if (! is_string($method))
-			return false;
+		$this->request = $this->request->withMethod($method);
 
-		if (! in_array($method, array('HEAD', 'GET', 'POST')))
-			return false;
-
-		$this->method = $method;
-
-		return true;
+		return $this;
 	}
 
-	public function setContentType($contentType) {
-		if (! is_string($contentType))
-			return false;
+	public function setUri($uri) {
+		$this->request = $this->request->withUri($uri);
 
-		$this->contentType = $contentType;
-
-		return true;
+		return $this;
 	}
 
-	public function setUserAgent($userAgent) {
-		if (! is_string($userAgent))
-			return false;
-
-		$this->userAgent = $userAgent;
-
-		return true;
+	public function setUrl($url) {
+		return $this->setUri($url);
 	}
 
-	public function setPostFields($postData) {
-		if (is_array($postData) && count($postData) > 0)
-			$postData = http_build_query($postData);
-
-		if (! is_string($postData))
-			return false;
-
-		$this->postFields = $postData;
-
-		return true;
+	public function getFollowRedirects() {
+		return $this->followRedirects;
 	}
 
 	public function setFollowRedirects($followRedirects) {
-		if (! is_bool($followRedirects))
-			return false;
+		$this->followRedirects = (bool)$followRedirects;
 
-		$this->followRedirects = $followRedirects;
-
-		return true;
+		return $this;
 	}
 
-	public function setMaxRequests($maxRequests) {
-		if (! is_numeric($maxRequests))
-			return false;
-
-		$this->maxRequests = (int)$maxRequests;
-
-		return true;
+	public function getMaxTries() {
+		return $this->maxTries;
 	}
 
-	public function setConnectTimeout($connectTimeout) {
-		if (! is_numeric($connectTimeout))
-			return false;
+	public function setMaxTries($maxTries) {
+		$this->maxTries = (int)$maxTries;
 
-		$this->connectTimeout = (int)$connectTimeout;
-
-		return true;
+		return $this;
 	}
 
-	public function setRequestTimeout($requestTimeout) {
-		if (! is_numeric($requestTimeout))
-			return false;
-
-		$this->requestTimeout = (int)$requestTimeout;
-
-		return true;
-	}
-
-	public function getResponseCode() {
-		return $this->responseCode;
-	}
-
-	public function getResponseContentType() {
-		return $this->responseContentType;
-	}
-
-	public function getRequestHeader() {
-		return $this->requestHeader;
-	}
-
-	public function getResponseFull() {
-		return $this->responseFull;
-	}
-
-	public function getResponseHeaderSize() {
-		return $this->responseHeaderSize;
-	}
-
-	public function getResponseHeader() {
-		return $this->responseHeader;
-	}
-
-	public function getResponseBody() {
-		return $this->responseBody;
-	}
-
-	public function getResponseErrno() {
-		return $this->responseErrno;
-	}
-
-	public function getResponseError() {
-		return $this->responseError;
-	}
-
-	public function send() {
-		if (! $this->url instanceof Uri)
-			return false;
+	public function send($request = NULL) {
+		if (! is_null($request) && $request instanceof \Psr\Http\Message\RequestInterface)
+			$this->request = $request;
 
 		$ch = curl_init();
+		curl_setopt_array($ch, $this->options);
 
-		curl_setopt($ch, CURLOPT_URL, (string)$this->url);
-		if (! is_null($this->userAgent))
-			curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
-		if ($this->url->getUserInfo() != '')
-			curl_setopt($ch, CURLOPT_USERPWD, $this->url->getUserInfo());
-		switch ($this->method) {
+		curl_setopt($ch, CURLOPT_URL, (string)$this->request->getUri());
+
+		$headers = array();
+		foreach ($this->request->getHeaders() as $k => $v)
+			$headers[] = $k.': '.$v[0];
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+		if ($this->request->getUri()->getUserInfo() != '')
+			curl_setopt($ch, CURLOPT_USERPWD, $this->request->getUri()->getUserInfo());
+
+		switch ($this->request->getMethod()) {
 			case 'HEAD':
 				curl_setopt($ch, CURLOPT_NOBODY, 1);
 				break;
@@ -197,67 +123,106 @@ class Client {
 				break;
 			case 'POST':
 				curl_setopt($ch, CURLOPT_POST, 1);
-				if (! is_null($this->postFields))
-					curl_setopt($ch, CURLOPT_POSTFIELDS, $this->postFields);
-				curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:', 'Connection: close', 'Content-Type: '.$this->contentType));
+				if ((string)$this->request->getBody() != '')
+					curl_setopt($ch, CURLOPT_POSTFIELDS, (string)$this->request->getBody());
 				break;
 		}
-		curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
+
 		curl_setopt($ch, CURLOPT_HEADER, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->connectTimeout);
-		curl_setopt($ch, CURLOPT_TIMEOUT, $this->requestTimeout);
 
 		$ret = curl_exec($ch);
 		$this->parseOutput($ch, $ret);
 
 		$i = 1;
-		while ((curl_errno($ch) == CURLE_COULDNT_CONNECT || curl_errno($ch) == CURLE_RECV_ERROR || curl_errno($ch) == CURLE_OPERATION_TIMEOUTED || curl_errno($ch) == CURLE_GOT_NOTHING) && $i < $this->maxRequests) {
+		while ((curl_errno($ch) == CURLE_COULDNT_CONNECT || curl_errno($ch) == CURLE_RECV_ERROR || curl_errno($ch) == CURLE_OPERATION_TIMEOUTED || curl_errno($ch) == CURLE_GOT_NOTHING) && $i < $this->maxTries) {
 			usleep(rand(1000000, 3000000));
 			$ret = curl_exec($ch);
 			$this->parseOutput($ch, $ret);
 			$i++;
 		}
 
-		$this->responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		$this->responseContentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+		$this->errno = curl_errno($ch);
+		$this->error = curl_error($ch);
 
-		$this->responseErrno = curl_errno($ch);
-		$this->responseError = curl_error($ch);
-
-		if (in_array($this->responseCode, array(301, 302)) && ! is_null($this->followRedirects) && $this->followRedirects === true) {
-			$this->setUrl(curl_getinfo($ch, CURLINFO_REDIRECT_URL));
+		if (substr($this->response->getStatusCode(), 0, 1) == 3 && $this->followRedirects) {
+			$this->setUri(new Uri(curl_getinfo($ch, CURLINFO_REDIRECT_URL)));
 			return $this->send();
 		}
 
 		curl_close($ch);
 
-		$this->reset();
+		if (! $ret || $this->errno == CURLE_COULDNT_CONNECT || $this->errno == CURLE_RECV_ERROR || $this->errno == CURLE_OPERATION_TIMEOUTED || $this->errno == CURLE_GOT_NOTHING)
+			throw new ClientException($this->error, $this->errno);
 
-		if (! $ret || $this->responseErrno == CURLE_COULDNT_CONNECT || $this->responseErrno == CURLE_RECV_ERROR || $this->responseErrno == CURLE_OPERATION_TIMEOUTED || $this->responseErrno == CURLE_GOT_NOTHING)
-			throw new ClientException($this->responseError, $this->responseErrno);
-
-		if ($this->method == 'HEAD')
-			return true;
-
-		return $this->responseBody;
+		return $this->response;
 	}
 
-	private function parseOutput(&$ch, $ret) {
-		if (! $ret)
+	public static function head($url, $queryString = NULL, array $options = array()) {
+		$c = new Client($url, 'HEAD', $options);
+		$c->setQueryString($queryString);
+		return $c->send();
+	}
+
+	public static function get($url, $queryString = NULL, array $options = array()) {
+		$c = new Client($url, 'GET', $options);
+		$c->setQueryString($queryString);
+		return $c->send();
+	}
+
+	public static function post($url, $postData = NULL, $queryString = NULL, array $options = array()) {
+		$c = new Client($url, 'POST', $options);
+		$c->setPostData($postData);
+		$c->setQueryString($queryString);
+		return $c->send();
+	}
+
+	public function setQueryString($queryString) {
+		if (is_array($queryString) || is_object($queryString))
+			$queryString = http_build_query($queryString);
+
+		if (is_string($queryString))
+			$this->request = $this->request->withUri($this->request->getUri()->withQuery(($this->request->getUri()->getQuery() != '')?$this->request->getUri()->getQuery().'&'.$queryString:$queryString));
+
+		return true;
+	}
+
+	public function setPostData($postData) {
+		if (is_array($postData) || is_object($postData))
+			$postData = http_build_query($postData);
+
+		if (is_string($postData)) {
+			$this->setMethod('POST');
+			$this->request = $this->request->withBody(\GuzzleHttp\Psr7\stream_for($postData));
+		}
+
+		return true;
+	}
+
+	private function parseOutput($ch, $ret) {
+		if (! is_resource($ch) || ! $ret)
 			return false;
 
-		$this->requestHeader = trim(curl_getinfo($ch, CURLINFO_HEADER_OUT));
-
-		$this->responseFull = $ret;
-		$this->responseHeaderSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-		if (is_numeric($this->responseHeaderSize) && $this->responseHeaderSize > 0) {
-			$this->responseHeader = trim(substr($ret, 0, $this->responseHeaderSize));
-			$this->responseBody = substr($ret, $this->responseHeaderSize);
+		$headers_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+		if (is_numeric($headers_size) && $headers_size > 0) {
+			$headers = trim(substr($ret, 0, $headers_size));
+			$body = substr($ret, $headers_size);
 		} else
-			list($this->responseHeader, $this->responseBody) = preg_split('/\r\n\r\n|\r\r|\n\n/', $ret, 2);
+			list($headers, $body) = preg_split('/\r\n\r\n|\r\r|\n\n/', $ret, 2);
+		if (! $body)
+			$body = '';
+
+		$header_lines = explode("\r\n", $headers);
+		$headers = array();
+		foreach ($header_lines as $header_line) {
+			$buf = explode(': ', $header_line);
+			if (count($buf) == 1)
+				continue;
+
+			$headers[$buf[0]] = $buf[1];
+		}
+
+		$this->response = new Response(curl_getinfo($ch, CURLINFO_HTTP_CODE), $headers, $body);
 
 		return true;
 	}
