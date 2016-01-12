94d819d1528ac8a8eb7a3f96e1fd9cfd37b92a65
Jérémy Desvages <jejem@phyrexia.org>
2016-01-12 14:19:09 +0100

    Releasing v2.3.1

--- src/Client.php
+++ src/Client.php
@@ -5,7 +5,7 @@
  * @author Jérémy 'Jejem' Desvages <jejem@phyrexia.org>
  * @copyright Jérémy 'Jejem' Desvages
  * @license The MIT License (MIT)
- * @version 2.3.0
+ * @version 2.3.1
 **/
 
 namespace Phyrexia\Http;
