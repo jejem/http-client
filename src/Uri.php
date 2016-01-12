b2d068046d2e4b8f0b4d0c5bfeb742c22a817284
Jérémy Desvages <jejem@phyrexia.org>
2016-01-12 12:56:58 +0100

    Releasing v2.3.0

--- src/Uri.php
+++ src/Uri.php
@@ -5,7 +5,7 @@
  * @author Jérémy 'Jejem' Desvages <jejem@phyrexia.org>
  * @copyright Jérémy 'Jejem' Desvages
  * @license The MIT License (MIT)
- * @version 2.2.0
+ * @version 2.3.0
 **/
 
 namespace Phyrexia\Http;
